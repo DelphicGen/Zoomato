97027f2c7d99d4b120fc3ebc694a807f9e3f94a8
DelphicGen <gennardokidam@gmail.com>
2020-10-04 13:17:33 +0700

    Remove commend from render method

--- src/components/landing-page/button/index.js
+++ src/components/landing-page/button/index.js
@@ -4,7 +4,6 @@ import Button from '@material-ui/core/Button'
 
 function Btn({text, onClick}) {
     return (
-        // <button onClick={onClick} className="bg-white text-blue-700 rounded-md px-5 py-1 ml-5 font-bold" data-testid="button">{text}</button>
         <Button
             variant="contained"
             color="primary"
